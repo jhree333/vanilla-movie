b43543c4f539fa71ef0107dbc17eb1303b4697e1
jhree333 <136793299+jhree333@users.noreply.github.com>
2024-08-03 13:19:51 +0900

    feat: 바닐라 자바스크립트 컴포넌트 단위 영화 웹사이트 제작

---
+++ src/store/about.js
@@ -0,0 +1,10 @@
+import { Store } from "../core"
+
+export default new Store({
+  photo: "https://cdn-icons-png.flaticon.com/512/149/149071.png",
+  name: "JHREE333 / JHREE",
+  email: "jhree333@gmail.com",
+  blog: "https://velog.io/@jhree333/posts",
+  github: "https://github.com/jhree333",
+  repository: "https://github.com/jhree333/vanillajs-movie-app",
+})
